e46bf026a912527125900fc80a06f44d46b699cc
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-01-21 05:07:35 +0000

    Update folder dataset

--- generate.sh
+++ generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir data
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json > data/train-v1.1.json
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json > data/dev-v1.1.json
+mkdir squad-v1.1
+
+curl https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json > data/squad-v1.1/train-v1.1.json
+curl https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json > data/squad-v1.1/dev-v1.1.json
